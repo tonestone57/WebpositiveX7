88f2ad385e5cc1bf50957afd1378f85ca2f37de7
tonestone57 <nickos.vs@gmail.com>
2026-01-18 06:13:48 +0200

    Delete webpositive/tests/mocks directory

--- webpositive/tests/mocks/File.cpp
+++
@@ -1,9 +0,0 @@
-#include "File.h"
-#include "Path.h"
-
-// We need to define this constructor separately to avoid
-// a circular dependency between File.h and Path.h
-BFile::BFile(const BPath& path, uint32 openMode)
-	: fStatus(B_OK)
-{
-}
